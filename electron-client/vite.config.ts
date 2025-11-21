b8cce59709e0d90aef0b70dfb5f2ee6c27052927
yamachu <yamachu.dev@gmail.com>
2025-11-21 12:26:13 +0900

    型が決まらなくてきついのでこうする

--- electron-client/vite.config.ts
+++ electron-client/vite.config.ts
@@ -1,11 +1,15 @@
+import { defineConfig } from "vite";
 import electron from "vite-plugin-electron/simple";
 
-export default {
+type ElectronPlugin = typeof import("vite-plugin-electron/simple.js").default;
+const electronPlugin = electron as unknown as ElectronPlugin;
+
+export default defineConfig({
   plugins: [
-    electron({
+    electronPlugin({
       main: {
         entry: "src/main.ts",
       },
     }),
   ],
-};
+});
